866165e49e097c3d7474985eceb0d639aa30c0c7
Diego Martinez <diegomrey1@gmail.com>
2024-05-07 00:23:22 +0200

    Show and close modal when click in a order

--- components/Kanban/Kanban.tsx
+++ components/Kanban/Kanban.tsx
@@ -1,10 +1,24 @@
 import { useOrders } from "@/contexts/Orders.context";
+import { Order } from "@/dtos/Order.dto";
+import { useState } from "react";
 import Column from "../Column";
 import Modal from "../Modal";
 import s from "./Kanban.module.scss";
 
 export default function Kanban() {
   const { orders } = useOrders();
+  const [selectedOrder, setSelectedOrder] = useState<Order | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = (order: Order) => {
+    setSelectedOrder(order);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setSelectedOrder(null);
+    setIsModalOpen(false);
+  };
 
   return (
     <section className={s["pk-kanban"]}>
@@ -12,6 +26,7 @@ export default function Kanban() {
         title="Pendiente"
         orders={orders.filter((i) => i.state === "PENDING")}
         state="IN_PROGRESS"
+        onClick={openModal}
         // onClick={() =>
         //   alert(
         //     "mmmmm..., deberias de modificar esto! tenemos que hacer que las ordenes lleguen hasta listo y se entreguen!"
@@ -28,7 +43,7 @@ export default function Kanban() {
         orders={orders.filter((i) => i.state === "READY")}
         state="DELIVERED"
       />
-      <Modal />
+      {isModalOpen ? <Modal onClose={closeModal} /> : <></>}
     </section>
   );
 }
--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -1,11 +1,17 @@
 import s from "./Modal.module.scss";
 
-export default function Modal() {
+export type ModalProps = {
+  onClose: () => void;
+};
+
+export default function Modal(props: ModalProps) {
   return (
     <>
       <div className={s["pk-overlay"]}>
         <div className={s["pk-modal"]} onClick={(e) => e.stopPropagation()}>
-          <button className={s["pk-closeButton"]}>X</button>
+          <button className={s["pk-closeButton"]} onClick={props.onClose}>
+            X
+          </button>
         </div>
       </div>
     </>
