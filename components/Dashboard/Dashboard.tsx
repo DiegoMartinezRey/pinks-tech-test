4689033fed48201d4a0911d23d8faa8dcd401c39
Diego Martinez <diegomrey1@gmail.com>
2024-05-10 01:24:54 +0200

    add order filter by id

--- components/Dashboard/Dashboard.tsx
+++ components/Dashboard/Dashboard.tsx
@@ -1,4 +1,5 @@
 import { useOrders } from "@/contexts/Orders.context";
+import { useState } from "react";
 import s from "./Dashboard.module.scss";
 
 interface Items {
@@ -13,15 +14,32 @@ interface Items {
 
 export default function Dashboard() {
   const { orders } = useOrders();
+  const [filter, setFilter] = useState<string>("");
 
   const sumPrice = (items: Items[]) => {
     return items.reduce((price, item) => price + item.price.amount, 0);
   };
 
+  const filteredOrders = orders.filter((order) =>
+    order.id.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
     <div className={s["pk-order"]}>
       <div className={s["pk-panel"]}>
         <h1>Historial de pedidos</h1>
+        <div className={s["pk-filter"]}>
+          <label htmlFor="orderFilter" className={s["pk-filter__label"]}>
+            Filtrar por ID:{" "}
+          </label>
+          <input
+            type="number"
+            placeholder="xxxxx"
+            value={filter}
+            onChange={(e) => setFilter(e.target.value)}
+            className={s["pk-filter__input"]}
+          />
+        </div>
         <div className={s["pk-panel__info"]}>
           <div className={s["pk-panel__order"]}>
             <h2>Orden</h2>
@@ -29,7 +47,7 @@ export default function Dashboard() {
             <h2>Precio total</h2>
             <h2>Estado pedido</h2>
           </div>
-          {orders.map((order) => (
+          {filteredOrders.map((order) => (
             <div className={s["pk-panel__order_column"]} key={order.id}>
               <div className={s["pk-panel__order"]}>
                 <h3 className={s["pk-panel__text"]}>{order.id}</h3>
